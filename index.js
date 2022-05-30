3d9e36c975cc2a8b9786bd097183ab714f6ae492
Jeremy Malczyk <jmalczyk@users.noreply.github.com>
2022-05-30 14:35:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 41.6303, lng: -73.3107 },
     zoom: 8,
   });
 }
