c33dd76c07e2281ab6ac56f2bba27daf98c90c06
jmalczyk <jmalczyk@gmail.com>
2022-05-30 23:43:08 -0400

    Formatting

--- index.js
+++ index.js
@@ -10,86 +10,91 @@ function initMap() {
 }
 function addLayer(geojson) {
   let bounds = new google.maps.LatLngBounds();
-  if(infowindow) {
-    infowindow.close()
+  if (infowindow) {
+    infowindow.close();
   } else {
     infowindow = new google.maps.InfoWindow();
   }
-  if(layer) {
+  if (layer) {
     layer.setMap(null);
-  } 
+  }
   layer = new google.maps.Data();
-  
-  
-  infowindow.close()
-  layer.setStyle({strokeWeight: 1, fillOpacity: 0});
-  layer.addListener('click', 
-                    function(f) {
-                      infowindow.close();
-                      let content = '<div>';
-                      f.feature.forEachProperty((val,key) => {
-                        content += `<div>
+
+  infowindow.close();
+  layer.setStyle({ strokeWeight: 1, fillOpacity: 0 });
+  layer.addListener("click", function (f) {
+    infowindow.close();
+    let content = "<div>";
+    f.feature.forEachProperty((val, key) => {
+      content += `<div>
                                       <strong class='infokey'>${key}</strong>
                                       <span class='infoval'>${val}</span>
                                     </div>`;
-                      });
-                      content += '</div>';
-                      infowindow.setContent(content);
-                      infowindow.open({
-                        anchor: new google.maps.Marker({position: f.latLng, 
-                                                        map:map,
-                                                        icon: {
-                                                          path: google.maps.SymbolPath.CIRCLE,
-                                                          scale: 0
-                                                        }}),
-                        map: map,
-                        shouldFocus: true,
-                      });
-                      
+    });
+    content += "</div>";
+    infowindow.setContent(content);
+    infowindow.open({
+      anchor: new google.maps.Marker({
+        position: f.latLng,
+        map: map,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 0,
+        },
+      }),
+      map: map,
+      shouldFocus: true,
+    });
 
-                      
-                      console.log(f)
-                  });
+    console.log(f);
+  });
   layer.addGeoJson(geojson);
   layer.setMap(map);
-  layer.forEach((f) => f.getGeometry().forEachLatLng((latLng) => bounds.extend(latLng)));
+  layer.forEach((f) =>
+    f.getGeometry().forEachLatLng((latLng) => bounds.extend(latLng))
+  );
   map.fitBounds(bounds);
 }
 
-function readFileAsArrayBuffer(file){
-  return new Promise(function(resolve,reject){
-      let fr = new FileReader();
+function readFileAsArrayBuffer(file) {
+  return new Promise(function (resolve, reject) {
+    let fr = new FileReader();
 
-      fr.onload = function(){
-          resolve({name: file.name, arrayBuffer: fr.result});
-      };
+    fr.onload = function () {
+      resolve({ name: file.name, arrayBuffer: fr.result });
+    };
 
-      fr.onerror = function(){
-          reject(fr);
-      };
+    fr.onerror = function () {
+      reject(fr);
+    };
 
-      fr.readAsArrayBuffer(file);
+    fr.readAsArrayBuffer(file);
   });
 }
 
-
 function loadShp(files, callback) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     let readers = [];
-    for(let i=0;i<files.length;i++) {
-      readers.push(readFileAsArrayBuffer(files[i]))
+    for (let i = 0; i < files.length; i++) {
+      readers.push(readFileAsArrayBuffer(files[i]));
     }
     Promise.all(readers).then(async (buffers) => {
       let geojson;
-      if(buffers.length > 1) {
+      if (buffers.length > 1) {
         const enc = new TextDecoder("utf-8");
         geojson = await shp.combine([
-          shp.parseShp(buffers.find(b=>b.name.endsWith('.shp')).arrayBuffer,
-                       enc.decode(buffers.find(b=>b.name.endsWith('.prj')).arrayBuffer)),
-          shp.parseDbf(buffers.find(b=>b.name.endsWith('.dbf')).arrayBuffer)
+          shp.parseShp(
+            buffers.find((b) => b.name.endsWith(".shp")).arrayBuffer,
+            enc.decode(buffers.find((b) => b.name.endsWith(".prj")).arrayBuffer)
+          ),
+          shp.parseDbf(
+            buffers.find((b) => b.name.endsWith(".dbf")).arrayBuffer
+          ),
         ]);
-      } else if(buffers.find(b=>b.name.endsWith('.zip'))) {
-        geojson = await shp.parseZip(buffers.find(b=>b.name.endsWith('.zip')).arrayBuffer)
+      } else if (buffers.find((b) => b.name.endsWith(".zip"))) {
+        geojson = await shp.parseZip(
+          buffers.find((b) => b.name.endsWith(".zip")).arrayBuffer
+        );
       }
       resolve(geojson);
     });
@@ -99,6 +104,3 @@ function loadShp(files, callback) {
 window.loadShp = loadShp;
 window.addLayer = addLayer;
 window.initMap = initMap;
-
-
-
